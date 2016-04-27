dba49c2991915b7c96ae71c10c1c2bf1cc8b184a
axd7832 <axd7832@g.rit.edu>
2016-04-27 14:39:11 -0400

    Server Version 1.0
    WOOT! MAKING THE WORLD A BETTER PLACE!

--- CardsServer.java
+++ CardsServer.java
@@ -7,13 +7,14 @@ import java.util.*;
  * @author Andrew Diana
  * @version 4-2-16
  */
-public class CardsServer {
+public class CardsServer implements Serializable{
    
    //Array of PrintWriters
    Vector <ObjectOutputStream> AllClients = new Vector <ObjectOutputStream>(); //Players + CardMaster
    Vector <BlackCard> blackArray  = new Vector<BlackCard>();  
    int playerNumber;
    int cardMaster;
+   String generic = null;
    
    /**
     * main method that calls the ChatServer constructor
@@ -30,8 +31,9 @@ public class CardsServer {
       ServerSocket ss = null;
       try {
          ss = new ServerSocket(16789);
-         System.out.println("Server Running...");
          Socket cs = null;
+         System.out.println("Server Running...");
+         System.out.println("Accepting Clients..");
          while(true){ 
             // run forever once up     
             cs = ss.accept(); 				// wait for connection
@@ -58,11 +60,12 @@ public class CardsServer {
       }
       public void setPlayerNumber(int num){
          this.playerNumber = num;
-         }
+      }
       public void run() {
          InputStream in;
          OutputStream out;
          String clientMsg;
+         String clientName = null;
          try {
             //Input
             synchronized(AllClients){
@@ -76,28 +79,44 @@ public class CardsServer {
             ObjectInputStream ois = new ObjectInputStream(in);
             
             AllClients.add(oos); //adds Client to the Vector list 
+            
+            
+            
             while( (to = ois.readObject() )  != null ){
+               
+            
+               //STRING SENT FROM CLIENT
                if (to instanceof String){ 
-                  clientMsg = (String)to;
-                  for(ObjectOutputStream client: AllClients){ //writes to all clients
-                     System.out.println(clientMsg);
-                     oos.writeObject(clientMsg);	//to client
-                     oos.flush(); //clears the remaining data in the PrintWriter
+                  //Sets Username
+                  if(clientName == null){
+                     clientName = (String)to;
+                     System.out.println("Client Name: "+ clientName);
+                  }
+                  //Username + Input
+                  else{
+                     clientMsg = (String)to;
+                     System.out.println("Client Message: "+ clientMsg);
+                     for(ObjectOutputStream client: AllClients){ //writes to all clients
+                        client.writeObject(clientName + ": " + clientMsg);	//to client
+                        client.flush(); //clears the remaining data in the PrintWriter
+                     }
                   }
                }
-               else if(to instanceof BlackCard){
-                  
-               }
-               else if(to instanceof WhiteCard){
-                  
-               }      			
+            //                else if(to instanceof BlackCard){
+            //                   
+            //                }
+            //                else if(to instanceof WhiteCard){
+            //                   
+            //                }      			
             }
          }
          catch( SocketException se ) { 
-            System.out.println("Client Disconnected..."); 
+            System.out.println("Client Disconnected...");
+            clientName = null; 
          }
          catch( IOException e ) { 
-            System.out.println("Connection Failed"); 
+            System.out.println("Connection Failed");
+            clientName = null;  
          }
          catch(ClassNotFoundException cnfe){
             System.out.println("Class Not Found!");
